672561a5d9840f9fdc1e644f64394fcef12e19ce
Nikita Loginov <nikitaloginov264@gmail.com>
2024-05-26 14:57:48 +0300

    added autoComplete.js and section:newseller and footer

--- main/main.js
+++ main/main.js
@@ -7,3 +7,69 @@ const swiper = new Swiper('.popular-swiper', {
         prevEl: '.swiper-arrows__prev',
     },
 });
+
+const autoCompleteJS = new autoComplete({
+    placeHolder: "e.g Bali, Indonesia",
+    data: {
+        src: ["Herat",
+        "Kabul",
+        "Kandahar",
+        "Molah",
+        "Rana",
+        "Shar",
+        "Sharif",
+        "Wazir Akbar Khan",
+        "Elbasan",
+        "Petran",
+        "Pogradec",
+        "Shkoder",
+        "Tirana",
+        "Ura Vajgurore",
+        "Algiers",
+        "Annaba",
+        "Azazga",
+        "Batna City",
+        "Blida",
+        "Bordj",
+        "Bordj Bou Arreridj",
+        "Bougara",
+        "Cheraga",
+        "Chlef",
+        "Constantine",
+        "Djelfa",
+        "Draria",
+        "El Tarf",
+        "Hussein Dey",
+        "Illizi",
+        "Jijel",
+        "Kouba",
+        "Laghouat",
+        "Oran",
+        "Ouargla",
+        "Oued Smar",
+        "Relizane",
+        "Rouiba",
+        "Saida",
+        "Souk Ahras",
+        "Tamanghasset",
+        "Tiaret",
+        "Tissemsilt",
+        "Tizi",
+        "Tizi Ouzou",
+        "Tlemcen"],
+        cache: true,
+    },
+    resultItem: {
+        highlight: true
+    },
+    events: {
+        input: {
+            selection: (event) => {
+                const selection = event.detail.selection.value;
+                autoCompleteJS.input.value = selection;
+            }
+        }
+    }
+});
+
+ScrollReveal().reveal('.headline');
